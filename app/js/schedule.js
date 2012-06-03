598cdd9c3b1c7c4d59b0a26e3d2a677cca9023be
Stephane Bisson <stephane.c.bisson@gmail.com>
2012-06-03 14:13:46 +0800

    display a fake schedule in a tab;e

---
+++ app/js/schedule.js
@@ -0,0 +1,22 @@
+runn.factory('schedule', function(){
+    var rest = function(){ return {activity: 'rest'}};
+    var run = function(timeR1){
+        return {
+            activity: 'run', 
+            desc: timeR1 + 'm@R1'
+        }
+    };
+    return {
+        query: function(){
+            return {
+                title: 'Half-Marathon Group B',
+                weeks: [
+                    {days: [run(35), rest(), run(20), rest(), run(25), run(50), rest()]},
+                    {days: [run(40), rest(), run(25), rest(), run(25), run(60), rest()]},
+                    {days: [run(35), rest(), run(25), rest(), run(20), run(70), rest()]},
+                    {days: [run(45), rest(), run(25), rest(), rest(), run(80), rest()]}
+                ]
+            };
+        }
+    };
+});
