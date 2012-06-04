6e682de951a45f44a6d4580ea88bb31cbb822e87
Stephane Bisson <stephane.c.bisson@gmail.com>
2012-06-04 22:34:44 +0800

    publish runn to gh-pages

--- js/schedule-controller.js
+++ js/schedule-controller.js
@@ -1,7 +1,20 @@
 runn.controller('ScheduleController',
 ['$scope', 'schedule', function($scope, schedule) {
     $scope.schedule = schedule.query();
-    $scope.click = function(){
-        $scope.popup = true;
+    $scope.click = function(day){
+        if (day.activity !== 'run') return;
+        
+        $.UIShowPopUp({
+            id: 'dayPopup',
+            title: 'Done!', 
+            message: 'Did you run ' + day.desc + ' successfully?', 
+            cancelUIButton: 'I tried but...', 
+            continueUIButton: 'Hell yeah!', 
+            callback: function() {
+                // stuff here
+                day.success = true;
+                $scope.$apply();
+            }
+        });
     };
 }]);
