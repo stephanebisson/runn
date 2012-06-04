66f8b633145e7b30f9b5407ebc01929c53f91c10
Stephane Bisson <stephane.c.bisson@gmail.com>
2012-06-04 20:41:20 +0800

    publish runn to gh-pages

--- js/schedule-controller.js
+++ js/schedule-controller.js
@@ -1,4 +1,7 @@
 runn.controller('ScheduleController',
 ['$scope', 'schedule', function($scope, schedule) {
     $scope.schedule = schedule.query();
+    $scope.click = function(){
+        $scope.popup = true;
+    };
 }]);
