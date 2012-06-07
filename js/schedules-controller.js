a690c320bfbbdd9774fddf221a16d68ce4a006a7
Stephane Bisson <stephane.c.bisson@gmail.com>
2012-06-07 10:05:49 +0800

    publish runn to gh-pages

---
+++ js/schedules-controller.js
@@ -0,0 +1,7 @@
+function SchedulesController($scope) {
+    $scope.schedules = [
+        {name: 'half-marathon group A', desc: 'half but fast!'},
+        {name: 'half-marathon group B', desc: 'still half, but slower'},
+        {name: 'half-marathon group C', desc: 'one has to start somewhere'}
+    ];
+}
